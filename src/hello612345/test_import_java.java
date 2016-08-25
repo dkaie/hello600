75f801c23e5539ac7f4ed7c3dfe38ab194d3252a
sdfadfdsf <adfdafdf@hotmail.com>
2016-08-25 20:53:50 +0800

    1050825 20:53上傳

---
+++ src/hello612345/test_import_java.java
@@ -0,0 +1,16 @@
+package hello612345;
+
+public class test_import_java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a = 10, b = 0;
+		//int c = a/b;
+		System.out.println("test import java"); //
+
+		
+		
+		
+	}
+
+}
